01780d5d03f44972cc253d499aaf6afb97709457
rubenvale31 <rubenvale31@ua.pt>
2021-11-24 00:16:12 +0000

    Boas, eu ja corrigi o erro do 'w'. Ou seja antes gravava a imagem da camara e agora já guarda a imagem da tela pintada.

--- ar_paint.py
+++ ar_paint.py
@@ -504,7 +504,7 @@ def main():
             current_month = datetime.datetime.strptime(month_number, "%m").strftime("%b")                               # Tranform current month number to name/text.
             current_time = datetime.datetime.now().strftime("%d_%H:%M:%S_%Y")                                           # Get current day of month, time and year.
             date_format = str(current_day_of_week) + '_' + str(current_month) + '_' + str(current_time)                 # Concatenate all date parameters.
-            cv2.imwrite('drawing_' + date_format + '.png', image)                                                       # Save image as png.
+            cv2.imwrite('drawing_' + date_format + '.png', white_window)                                                       # Save image as png.
         elif (key == ord('o')) or (key == ord('O')):                                                                    # Check if user pressed the 'o' key.
             print(Fore.YELLOW + Style.BRIGHT + 'Drawing circle.' + Style.RESET_ALL)
             circleX = last_coordinates[0]                                                                               # Saves centroid x position
